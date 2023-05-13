328ed05c20a114284f136a3db8dd9d8f39c9fa30
saladball <38815353+saladball@users.noreply.github.com>
2023-05-14 00:48:51 +0900

    news app add

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import geoip2.database
 import socket
 import re
+import urllib.parse
+import requests
+import os
+from flask_paginate import Pagination, get_page_args
+from datetime import datetime, timedelta
 from flask import Flask, render_template, request
 from modules.County_Codes import country_code_to_korean
 
@@ -63,6 +68,48 @@ def ascii_to_char():
             if ascii_code >= 0 and ascii_code <= 127:
                 characters += chr(ascii_code)
     return render_template('ascii_to_char.html', characters=characters)
+
+@app.route('/utf8_decoder', methods=['GET', 'POST'])
+def utf8_decoder():
+    decoded_string = ''
+    if request.method == 'POST':
+        encoded_string = request.form.get('encoded_string')
+        decoded_string = urllib.parse.unquote(encoded_string)
+    return render_template('utf8_decoder.html', decoded_string=decoded_string)
+
+@app.route('/news', methods=['GET'])
+def news():
+    # Get page info from request
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    
+    # Bing Search v7 API endpoint
+    url = "https://api.cognitive.microsoft.com/bing/v7.0/news/search"
+    headers = {"Ocp-Apim-Subscription-Key": os.getenv("ec6fd8d4f6cb4263be9c7062fd398f91")}
+    params = {
+        "q": "해킹 OR 해커 OR 멀웨어 OR 취약점",
+        "count": 50,
+        "freshness": "Day",
+        "textFormat": "Raw",
+        "safeSearch": "Off"
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    news_data = response.json()["value"]
+
+    # Convert published time to datetime object
+    for news in news_data:
+        news["datePublished"] = datetime.strptime(news["datePublished"], "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    # Paginate news
+    total = len(news_data)
+    pagination_news = news_data[offset: offset + per_page]
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+
+    return render_template('news.html', news=pagination_news, page=page,
+                           per_page=per_page, pagination=pagination)
 
 if __name__ == '__main__':
     app.run()
